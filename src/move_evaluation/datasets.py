aa6963aed1ba08a7a04643d3430346f4375a85f2
Kamil2601 <mich.kamil2601@gmail.com>
2023-04-25 23:56:43 +0200

    Testing model against random agent

--- src/move_evaluation/datasets.py
+++ src/move_evaluation/datasets.py
@@ -2,12 +2,18 @@ from torch.utils.data import Dataset
 import torch
 from board_representation import move_to_tensor
 import pandas as pd
+import multiprocessing as mp
+
+
+def _move_to_tensor_tuple(move):
+    return move_to_tensor(move[0], move[1])
 
 
 class MoveAsTensorDataset(Dataset):
     def __init__(self, moves_df: pd.DataFrame, position_col="position", move_col="move", sentiment_col="sentiment"):
         self.sentiments = [torch.tensor([s], dtype=torch.float32) for s in moves_df[sentiment_col]]
-        self.moves = [move_to_tensor(row[position_col], row[move_col]) for _, row in moves_df.iterrows()]
+        moves_tuples = list(zip(moves_df[position_col], moves_df[move_col]))
+        self.moves = list(map(_move_to_tensor_tuple, moves_tuples))
 
     def __getitem__(self, index):
         return self.moves[index], self.sentiments[index]
