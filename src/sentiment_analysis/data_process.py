7c534200a02e8b8e925ecc71264d0a632246d603
Kamil2601 <mich.kamil2601@gmail.com>
2023-06-13 21:24:30 +0200

    update

--- src/sentiment_analysis/data_process.py
+++ src/sentiment_analysis/data_process.py
@@ -4,26 +4,34 @@ import spacy
 from spacy.symbols import ORTH
 from sklearn.model_selection import train_test_split
 from langdetect import detect_langs, detect
+import string
+from nltk.tokenize import word_tokenize
 
 
-move_regex = re.compile("([Oo0](-[Oo0]){1,2}|[KQRBN]?[a-h]?[1-8]?[x-]?[a-h][1-8](\=[QRBN])?[+#]?[!?]*(\s(1-0|0-1|1\/2-1\/2))?)")
+
+move_regex = re.compile(
+    "([Oo0](-[Oo0]){1,2}|[KQRBN]?[a-h]?[1-8]?[x-]?[a-h][1-8](\=[QRBN])?[+#]?[!?]*(\s(1-0|0-1|1\/2-1\/2))?)"
+)
 numbered_move_regex = re.compile("\d+\.{1,3}\s*(__move__\s*){0,1}(__move__)")
-full_regex = re.compile("\d{1,}\.{1,3}\s?(([Oo0]-[Oo0](-[Oo0])?|[KQRBN]?[a-h]?[1-8]?[x-]?[a-h][1-8]?(\=[QRBN])?[+#]?[!?]*)(\s?\{.+?\})?(\s(1-0|0-1|1\/2-1\/2))?\s){1,2}")
+full_regex = re.compile(
+    "\d{1,}\.{1,3}\s?(([Oo0]-[Oo0](-[Oo0])?|[KQRBN]?[a-h]?[1-8]?[x-]?[a-h][1-8]?(\=[QRBN])?[+#]?[!?]*)(\s?\{.+?\})?(\s(1-0|0-1|1\/2-1\/2))?\s){1,2}"
+)
 
 punctuation_regex = r"[\"#\$%&\'\(\)\*\+,-\./:;<=>\@\[\\\]\^_`{\|}~ 0-9]+"
 
-nlp = spacy.load("en_core_web_sm", disable = ['parser','ner'])
+# nlp = spacy.load("en_core_web_sm", disable=["parser", "ner"])
 
 
 notation_token = "__notation__"
 move_token = "__move__"
 
-nlp.tokenizer.add_special_case(notation_token, [{ORTH: notation_token}])
-nlp.tokenizer.add_special_case(move_token, [{ORTH: move_token}])
+# nlp.tokenizer.add_special_case(notation_token, [{ORTH: notation_token}])
+# nlp.tokenizer.add_special_case(move_token, [{ORTH: move_token}])
+
 
 def is_english(text):
     try:
-        return detect(text) == 'en'
+        return detect(text) == "en"
     except:
         return False
 
@@ -39,6 +47,7 @@ def replace_notation(comment: str):
     com = re.sub("[+-]?\d+[.]\d+([/]\d*[.]?\d*)", "", com)
     # com = re.sub("\d+", "", com)
     return com
+
 
 def remove_notation(comment: str):
     comment = replace_notation(comment)
@@ -56,7 +65,10 @@ def remove_useless(comment: str):
 
 
 def token_filter(token):
-    return not re.search(punctuation_regex, token.lemma_) or re.match(notation_token, token.lemma_)
+    return not re.search(punctuation_regex, token.lemma_) or re.match(
+        notation_token, token.lemma_
+    )
+
 
 def get_lemma(token):
     return token.lemma_.lower()
@@ -69,45 +81,88 @@ def token_to_text_or_lemma(token, vocab):
         return token.lemma_
     return None
 
+
 def doc_to_text_or_lemma_tokens(doc, vocab):
     tokens = [token_to_text_or_lemma(token, vocab) for token in doc]
     return [token for token in tokens if token]
 
+
 def doc_to_lemma_tokens(doc, vocab):
     return [token.lemma_ for token in doc if token.lemma_ in vocab]
+
 
 def doc_to_tokens(doc, vocab):
     return [token.text for token in doc if token.text in vocab]
 
 
-def add_comments_with_removed_quality_marks(df_comments: pd.DataFrame, comment_col="comment"):
+def remove_bad_tokens(preprocessed_comment):
+    bad_tokens = set(["[", "]", "csl", "cal", "\\"] + list("bcdfghjklmnpqrstvwxyz"))
+    return [token for token in preprocessed_comment if token not in bad_tokens]
+
+
+def add_comments_with_removed_quality_marks(
+    df_comments: pd.DataFrame, comment_col="comment"
+):
     """
-        For every comment with quality mark, add the same comment with removed quality mark, and the same sentiment
-        e.g. "! good move" 1 -> "good move" 1
+    For every comment with quality mark, add the same comment with removed quality mark, and the same sentiment
+    e.g. "! good move" 1 -> "good move" 1
     """
 
     def remove_quality_mark(comment):
-        return re.sub("^[!?]{1,2}[ ]","", comment)
+        return re.sub("^[!?]{1,2}[ ]", "", comment)
 
-    df_comments_copy = df_comments[df_comments[comment_col].str.match("^[!?]{1,2}[ ]")].copy()
-    df_comments_copy[comment_col] = df_comments_copy[comment_col].map(remove_quality_mark)
+    df_comments_copy = df_comments[
+        df_comments[comment_col].str.match("^[!?]{1,2}[ ]")
+    ].copy()
+    df_comments_copy[comment_col] = df_comments_copy[comment_col].map(
+        remove_quality_mark
+    )
 
     return pd.concat([df_comments, df_comments_copy], axis=0, ignore_index=True)
 
 
-def spacy_tokenize(comments, vocab, doc_to_tokens):
-    nlp = spacy.load("en_core_web_sm", disable = ['parser','ner'])
-    docs = [t for t in nlp.pipe(comments, n_process=16, batch_size=128)]
-    tokenized_comments = [doc_to_tokens(doc, vocab) for doc in docs]
+def spacy_tokenize(comments, vocab, doc_to_tokens, split = 200000):
+    nlp = spacy.load("en_core_web_sm", disable=["parser", "ner"])
+
+    tokenized_comments = []
+
+    comments = list(comments)
+
+    for i in range(0,len(comments), split):
+        docs = [t for t in nlp.pipe(comments[i:i+split], n_process=16, batch_size=128)]
+        tokenized_comments += [doc_to_tokens(doc, vocab) for doc in docs]
 
     return tokenized_comments
+
+
+def nltk_tokenize(comments, vocab, doc_to_tokens):
+    return [list(filter(lambda w: w in vocab, word_tokenize(com.lower()))) for com in comments]
+
+
+
 
 def shuffle_and_sort(df: pd.DataFrame, comment_col):
     df = df.sample(frac=1)
     return df.sort_values(by=comment_col, key=lambda x: x.str.len())
 
-def df_train_test_split(comments_df, comment_col, test_size=None, train_size=None, random_state=None, shuffle=True, stratify=None):
-    train_df, test_df = train_test_split(comments_df, test_size=test_size, train_size=train_size, random_state=random_state, shuffle=shuffle, stratify=stratify)
+
+def df_train_test_split(
+    comments_df,
+    comment_col,
+    test_size=None,
+    train_size=None,
+    random_state=None,
+    shuffle=True,
+    stratify=None,
+):
+    train_df, test_df = train_test_split(
+        comments_df,
+        test_size=test_size,
+        train_size=train_size,
+        random_state=random_state,
+        shuffle=shuffle,
+        stratify=stratify,
+    )
 
     train_df = train_df.sort_values(by=comment_col, key=lambda x: x.str.len())
     test_df = test_df.sort_values(by=comment_col, key=lambda x: x.str.len())
@@ -115,9 +170,19 @@ def df_train_test_split(comments_df, comment_col, test_size=None, train_size=Non
     return train_df, test_df
 
 
-def prepare_data_for_sentiment_analysis_training(df_moves: pd.DataFrame, vocab, comment_col="comment", sentiment_col="sentiment", doc_to_tokens = doc_to_lemma_tokens, min_len = 2, max_len = 100):
-    #Filter moves, leave only with 0/1 sentiment
-    df_comments = df_moves[df_moves[sentiment_col].isin([0,1])][[comment_col, sentiment_col]]
+def prepare_data_for_sentiment_analysis_training(
+    df_moves: pd.DataFrame,
+    vocab,
+    comment_col="comment",
+    sentiment_col="sentiment",
+    doc_to_tokens=doc_to_text_or_lemma_tokens,
+    min_len=2,
+    max_len=100,
+):
+    # Filter moves, leave only with 0/1 sentiment
+    df_comments = df_moves[df_moves[sentiment_col].isin([0, 1])][
+        [comment_col, sentiment_col]
+    ]
     df_comments = add_comments_with_removed_quality_marks(df_comments, comment_col)
 
     # remove chess notation and some punctuation
@@ -125,25 +190,135 @@ def prepare_data_for_sentiment_analysis_training(df_moves: pd.DataFrame, vocab, 
     # df_comments[comment_col] = [com.lower() for com in df_comments[comment_col]]
 
     # tokenize with Spacy
-    df_comments['preprocessed_comment'] = spacy_tokenize(df_comments[comment_col], vocab, doc_to_tokens)
+    df_comments["preprocessed_comment"] = spacy_tokenize(
+        df_comments[comment_col], vocab, doc_to_tokens
+    )
 
-    df_comments = df_comments[df_comments['preprocessed_comment'].map(len).between(min_len, max_len)]
+    df_comments["preprocessed_comment"] = df_comments["preprocessed_comment"].map(
+        remove_bad_tokens
+    )
 
-    df_comments = shuffle_and_sort(df_comments, 'preprocessed_comment')
+    df_comments = df_comments[
+        df_comments["preprocessed_comment"].map(len).between(min_len, max_len)
+    ]
+
+    df_comments = shuffle_and_sort(df_comments, "preprocessed_comment")
 
     return df_comments
 
 
-def prepare_data_for_sentiment_analysis_prediction(df_moves: pd.DataFrame, vocab, comment_col="comment", prep_comment_col="preprocessed_comment", doc_to_tokens = doc_to_lemma_tokens, min_len = 2, max_len = 100):
+def prepare_data_for_sentiment_analysis_prediction(
+    df_moves: pd.DataFrame,
+    vocab,
+    comment_col="comment",
+    prep_comment_col="preprocessed_comment",
+    doc_to_tokens=doc_to_text_or_lemma_tokens,
+    min_len=2,
+    max_len=100,
+):
     # remove chess notation and some punctuation
     df_moves[prep_comment_col] = [remove_useless(com) for com in df_moves[comment_col]]
     # df_moves[prep_comment_col] = [com.lower() for com in df_moves[comment_col]]
 
     # tokenize with Spacy
-    df_moves[prep_comment_col] = spacy_tokenize(df_moves[prep_comment_col], vocab, doc_to_tokens)
+    df_moves[prep_comment_col] = spacy_tokenize(
+        df_moves[prep_comment_col], vocab, doc_to_tokens
+    )
+
+    df_moves[prep_comment_col] = df_moves[prep_comment_col].map(remove_bad_tokens)
 
     df_moves = df_moves[df_moves[prep_comment_col].map(len).between(min_len, max_len)]
 
     df_moves = shuffle_and_sort(df_moves, prep_comment_col)
 
     return df_moves
+
+
+class DataProcessing:
+    def __init__(
+        self,
+        vocab,
+        doc_to_tokens=doc_to_text_or_lemma_tokens,
+        min_len=2,
+        max_len=100,
+        preprocess_fun=remove_useless,
+        postprocess_fun=remove_bad_tokens,
+        tokenize_fun = spacy_tokenize
+    ) -> None:
+        self.vocab = vocab
+        self.doc_to_tokens = doc_to_tokens
+        self.min_len = min_len
+        self.max_len = max_len
+        self.preprocess_fun = preprocess_fun
+        self.postprocess_fun = postprocess_fun
+        self.tokenize_fun = tokenize_fun
+
+    def prepare_data_for_sentiment_analysis_training(
+        self,
+        df_moves: pd.DataFrame,
+        comment_col="comment",
+        sentiment_col="sentiment",
+        prep_comment_col="preprocessed_comment",
+    ):
+        # Filter moves, leave only with 0/1 sentiment
+        df_comments = df_moves[df_moves[sentiment_col].isin([0, 1])][
+            [comment_col, sentiment_col]
+        ]
+
+        df_comments = add_comments_with_removed_quality_marks(df_comments, comment_col)
+
+        # remove chess notation and some punctuation
+        if self.preprocess_fun:
+            df_comments[prep_comment_col] = df_comments[comment_col].map(
+                self.preprocess_fun
+            )
+
+
+        # tokenize with Spacy
+        df_comments[prep_comment_col] = self.tokenize_fun(
+            df_comments[prep_comment_col], self.vocab, self.doc_to_tokens
+        )
+
+        if self.postprocess_fun:
+            df_comments[prep_comment_col] = df_comments[prep_comment_col].map(
+                self.postprocess_fun
+            )
+
+
+        df_comments = df_comments[
+            df_comments[prep_comment_col]
+            .map(len)
+            .between(self.min_len, self.max_len + 1)
+        ]
+
+        df_comments = shuffle_and_sort(df_comments, prep_comment_col)
+
+        return df_comments
+
+    def prepare_data_for_sentiment_analysis_prediction(
+        self,
+        df_moves: pd.DataFrame,
+        comment_col="comment",
+        prep_comment_col="preprocessed_comment",
+    ):
+
+        if self.preprocess_fun:
+            df_moves[prep_comment_col] = df_moves[comment_col].map(self.preprocess_fun)
+
+        # tokenize with Spacy
+        df_moves[prep_comment_col] = self.tokenize_fun(
+            df_moves[prep_comment_col], self.vocab, self.doc_to_tokens
+        )
+
+        if self.postprocess_fun:
+            df_moves[prep_comment_col] = df_moves[prep_comment_col].map(
+                self.postprocess_fun
+            )
+
+        df_moves = df_moves[
+            df_moves[prep_comment_col].map(len).between(self.min_len, self.max_len + 1)
+        ]
+
+        df_moves = shuffle_and_sort(df_moves, prep_comment_col)
+
+        return df_moves
